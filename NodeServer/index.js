be8e9851289283d80b8077d84c5d9b0fbf512e4e
Saulguz1 <guzmanmsaul@gmail.com>
2021-08-07 10:35:00 -0600

    Ejemplo 1 Server

---
+++ NodeServer/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const cors = require('cors');
+var corsOptions = { origin: true, optionsSuccessStatus: 200 };
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+var port = 9000;
+app.listen(port);
+console.log('Listening on port', port);
+
+//--------------------------------------------------Prueba---------------------------------------
+
+app.get('/', function (req, res) {
+
+  res.json({ mensaje: 'Hola semi 1 - a'})
+
+});
