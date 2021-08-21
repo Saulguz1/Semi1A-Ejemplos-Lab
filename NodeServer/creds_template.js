cfe4e86c150a84bb9afd244580efe0eab2ab78bd
Saulguz1 <guzmanmsaul@gmail.com>
2021-08-21 12:17:47 -0600

    S3

---
+++ NodeServer/creds_template.js
@@ -0,0 +1,9 @@
+let aws_keys = {
+    s3: {
+        region: 'us-east-2',
+        accessKeyId: "",
+        secretAccessKey: "",
+        //apiVersion: '2006-03-01',
+    }
+}
+module.exports = aws_keys
--- NodeServer/index.js
+++ NodeServer/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 const cors = require('cors');
+var uuid = require('uuid');
+const aws_keys = require('./creds');
 var corsOptions = { origin: true, optionsSuccessStatus: 200 };
 app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
@@ -10,6 +12,13 @@ var port = 9000;
 app.listen(port);
 console.log('Listening on port', port);
 
+
+//instanciamos el sdk
+var AWS = require('aws-sdk');
+//instanciamos los servicios a utilizar con sus respectivos accesos.
+const s3 = new AWS.S3(aws_keys.s3);
+
+
 //--------------------------------------------------Prueba---------------------------------------
 
 app.get('/', function (req, res) {
@@ -17,3 +26,87 @@ app.get('/', function (req, res) {
   res.json({ mensaje: 'Hola semi 1 - A'})
 
 });
+
+
+//--------------------------------------------------ALMACENAMIENTO---------------------------------------
+
+//subir foto en s3  EN EL PROYECTO SOLO SE SUBIRAN IMAGENES (JPG,PNG) Y ARCHIVOS PDF
+app.post('/subirfoto', function (req, res) {
+
+  var id = req.body.id;
+  var foto = req.body.foto;     //base64
+  //carpeta y nombre que quieran darle a la imagen
+
+  var nombrei = "fotos/" + id + ".jpg";
+
+  //se convierte la base64 a bytes
+  let buff = new Buffer.from(foto, 'base64');
+
+
+  
+  const params = {
+    Bucket: "ejemplosemiarchivos",
+    Key: nombrei,
+    Body: buff,
+    ContentType: "image",
+    ACL: 'public-read'
+  };
+
+
+
+  
+  const putResult = s3.putObject(params).promise();
+  res.json({ mensaje: putResult })
+
+
+
+});
+
+//subir pdf en s3
+app.post('/subirfile', function (req, res) {
+
+  var nombre = req.body.nombre;
+  var pdf = req.body.pdf;  //base 64
+  //carpeta y nombre que quieran darle al pdf
+  var nombrei = "files/" + nombre +uuid.v4()+ ".pdf";
+  //se convierte la base64 a bytes
+  let buff = new Buffer.from(pdf, 'base64');
+  const params = {
+    Bucket: "ejemplosemiarchivos",
+    Key: nombrei,
+    Body: buff,
+    ACL: 'public-read'
+  };
+
+  s3.upload(params, function sync(err, data) {
+     if (err) {
+       res.status(500).send(err)
+     } else {
+      console.log(data.Location);  
+      res.status(200).send(data);
+                        
+   }}); 
+
+
+});
+
+//NO ES NECESARIO PARA SU PROYECTO PERO PUEDEN USARLO obtener objeto en s3
+app.post('/obtenerfoto', function (req, res) {
+  var id = req.body.id;
+  //direcccion donde esta el archivo a obtener
+  var nombrei = "fotos/" + id + ".jpg";
+  var getParams = {
+    Bucket: 'ejemplosemiarchivos',
+    Key: nombrei
+  }
+  s3.getObject(getParams, function (err, data) {
+    if (err)
+      res.json({ mensaje: "error" })
+    //de bytes a base64
+    var dataBase64 = Buffer.from(data.Body).toString('base64');
+    res.json({ mensaje: dataBase64 })
+
+  });
+
+});
+
